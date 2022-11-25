0ab80c674e993037fc23ce2c41b0fab522d724ce
Gio Katamadze <giokat.inbox@gmail.com>
2022-11-25 14:30:41 +0400

    #4 Create title and IP / Domain search bar.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './reset.css';
 import './App.css';
-import background from './images/pattern-bg.png'
+import arrow from './images/icon-arrow.svg'
 
 // https://geo.ipify.org/api/v2/country,city?apiKey=at_c0XRjdWW9XdRj5WOWrHPcbxWMm9n3&ipAddress=8.8.8.8
 
 
 function App() {
   return (
-    <div className="Wrapper">
-      <div>
-        <img className='Background' src={ background } alt=""/>
+    <div className="wrapper">
+      <div className='background'>
+        <h1>IP Address Tracker</h1>
+
+        <form
+            // onSubmit={handleSubmit}
+            autoComplete="off"
+            className="w-full flex"
+          >
+            <input
+              className="inputBar"
+              type="text"
+              name="ipaddress"
+              id="ipaddress"
+              placeholder="Search for any IP address or domain"
+              // value={ipAddress}
+              // onChange={(e) => setIpAddress(e.target.value)}
+            />
+            <button type="submit" className="inputButton">
+              <img src={arrow} alt="" />
+            </button>
+          </form>
+
       </div>
     </div>
   );
