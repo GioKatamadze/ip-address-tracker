93dae97aea427cf209aec05feb1f13a19f72c8c7
Gio Katamadze <giokat.inbox@gmail.com>
2022-11-25 16:25:38 +0400

    #5 Create GeoLocation results display.

--- src/App.js
+++ src/App.js
@@ -7,28 +7,62 @@ import arrow from './images/icon-arrow.svg'
 
 function App() {
   return (
-    <div className="wrapper">
+    <div className='wrapper'>
       <div className='background'>
         <h1>IP Address Tracker</h1>
 
+        
+
         <form
             // onSubmit={handleSubmit}
-            autoComplete="off"
-            className="w-full flex"
+            autoComplete='off'
           >
             <input
-              className="inputBar"
-              type="text"
-              name="ipaddress"
-              id="ipaddress"
-              placeholder="Search for any IP address or domain"
+              className='inputBar'
+              type='text'
+              name='ipaddress'
+              id='ipaddress'
+              placeholder='Search for any IP address or domain'
               // value={ipAddress}
               // onChange={(e) => setIpAddress(e.target.value)}
             />
-            <button type="submit" className="inputButton">
-              <img src={arrow} alt="" />
+            <button type='submit' className='inputButton'>
+              <img src={arrow} alt='' />
             </button>
           </form>
+
+
+
+
+          <div className='resultsDisplay' >
+
+            <div className='displayBox' >
+              <p className='displayTitle' >IP ADDRESS</p>
+              <h2 className='displayValue' >192.212.174.101</h2>
+            </div>
+
+            <div className='hr'></div>
+
+            <div className='displayBox' >
+              <p className='displayTitle' >LOCATION</p>
+              <h2 className='displayValue' >Brooklyn, NY 10001</h2>
+            </div>
+
+            <div className='hr'></div>
+
+            <div className='displayBox' >
+              <p className='displayTitle' >TIMEZONE</p>
+              <h2 className='displayValue' >UTC -05:00</h2>
+            </div>
+
+            <div className='hr'></div>
+
+            <div className='displayBox' >
+              <p className='displayTitle' >ISP</p>
+              <h2 className='displayValue' >SpaseX Starlink</h2>
+            </div>
+
+          </div>
 
       </div>
     </div>
